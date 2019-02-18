d53ebb7d40fecea51bb38ed01d9ac2ca210adc6c
Vlad <vovaturcan4@gmail.com>
2019-02-18 18:39:23 +0200

    initiated the project, created the folders and added dummy data

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import dummyData from "./dummy-data";
+
+class App extends Component {
+  render() {
+    console.log(dummyData);
+    return (
+      <div>
+        <h1>Project</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
